09ac9bad95870cf56a3805f36ba7ba2c7880f42e
BuTomer <bu.tomer@gmail.com>
2023-11-06 18:05:57 +0200

    biz-cards setup

--- index.ts
+++ index.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import { config } from "dotenv";
 config(); //load all the values from .env
-import filmsRouter from "./routes/films";
-
+import usersRouter from "./routes/users";
+import { logger } from "./middleware/logger";
 const app = express();
 
 //add an express middleware that uses JSON.parse(body)
 app.use(express.json());
+app.use(logger);
 
-app.use("/api/v1/movies", filmsRouter);
+app.use("/api/v1/users", usersRouter);
 
 const PORT = process.env.EXPRESS_PORT;
 
