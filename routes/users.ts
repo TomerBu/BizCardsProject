ae776b16db2b8db6bc152f6bf7594efd24974ffe
Tomer <tomer.bu@gmail.com>
2023-11-27 11:44:59 +0200

    targil 1

--- routes/users.ts
+++ routes/users.ts
@@ -5,6 +5,7 @@ import { validateLogin, validateUser } from "../middleware/validate-schema";
 import { verifyAdmin } from "../middleware/verify-admin";
 import { userService } from "../service/user.service";
 import { verifyUserOrAdmin } from "../middleware/verify-user-or-admin";
+import { verifyUser } from "../middleware/verify-user";
   
 const router = Router();
 
@@ -53,6 +54,28 @@ router.get("/:id", verifyUserOrAdmin, async (req, res, next) => {
   }
 });
 
+//Update user by id:
+router.put("/:id", verifyUser, validateUser, async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const body = req.body as IUser;
+
+    const updatedUser = await User.findByIdAndUpdate(id, body, {
+      new: true,
+      /*
+      By default, findOneAndUpdate() returns the document as it was <b>before</b> update
+      Setting new: true, returns the document <b>after</b> update was applied
+      */
+    });
+    
+    if (!updatedUser) {
+      return res.status(404).json({ message: `user with id: ${id} Not found` });
+    }
+    res.json(updatedUser);
+  } catch (e) {
+    next(e);
+  }
+});
 export default router;
 
 //try{const token = service.login(req.body);  res.status(200).json({token: token})}
