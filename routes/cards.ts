c3c6330bb14b885bd78893c971aece6c15bef775
Tomer <tomer.bu@gmail.com>
2023-11-27 12:42:06 +0200

    targil 5 my-cards

--- routes/cards.ts
+++ routes/cards.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { Card } from "../db/model/card.model";
+import { verifyToken } from "../middleware/verify-token";
 
 const router = Router();
 
@@ -7,6 +8,18 @@ const router = Router();
 router.get("/", async (req, res, next) => {
   try {
     const cards = await Card.find();
+    res.json(cards);
+  } catch (e) {
+    next(e);
+  }
+});
+
+//Get my cards
+router.get("/my-cards", verifyToken, async (req, res, next) => {
+  try {
+    const userId = req.user?.id;
+    
+    const cards = await Card.find({ user_id: userId });
     res.json(cards);
   } catch (e) {
     next(e);
