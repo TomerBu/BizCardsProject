5b5dfa32df47e8a789d2fd1f537ca0e4a165bbcd
Tomer <tomer.bu@gmail.com>
2023-11-27 13:09:05 +0200

    targil 6 post a card - requires business user

--- routes/cards.ts
+++ routes/cards.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { Card } from "../db/model/card.model";
 import { verifyToken } from "../middleware/verify-token";
+import { validateCard } from "../middleware/validate-schema";
+import { verifyIsBusiness } from "../middleware/verify-is-business";
 
 const router = Router();
 
@@ -18,9 +20,20 @@ router.get("/", async (req, res, next) => {
 router.get("/my-cards", verifyToken, async (req, res, next) => {
   try {
     const userId = req.user?.id;
-    
+
     const cards = await Card.find({ user_id: userId });
     res.json(cards);
+  } catch (e) {
+    next(e);
+  }
+});
+
+//Post a card
+router.post("/", verifyIsBusiness, validateCard, async (req, res, next) => {
+  try {
+    const userId = req.user?.id;
+    const card = await Card.create({ ...req.body, user_id: userId });
+    res.status(201).json(card);
   } catch (e) {
     next(e);
   }
